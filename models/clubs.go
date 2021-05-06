584188c0e7b9082c8e451f34d7204235a47f2f8f
Krishap-s <krishap.s.sreenivasan@gmail.com>
2021-05-06 17:10:44 +0530

    feat: add websocket notifications if club details are updated and if a user joins or leaves a club

--- models/clubs.go
+++ models/clubs.go
@@ -1,6 +1,13 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"context"
+	"encoding/json"
+	"github.com/Krishap-s/keats-backend/redisclient"
+	"github.com/go-pg/pg/v10"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
 
 // Room represents a room in the database
 type Club struct {
@@ -12,4 +19,21 @@ type Club struct {
 	HostID   uuid.UUID `pg:",type:uuid" json:"host_id"`
 	PageSync bool      `pg:",use_zero" json:"page_sync"`
 	Private  bool      `pg:",use_zero" json:"private"`
+}
+
+var _ pg.AfterUpdateHook = (*Club)(nil)
+
+// AfterUpdate hook publishes club update notifications to websocket users
+func (c *Club) AfterUpdate(ctx context.Context) error {
+	rdb, err := redisclient.GetRedisClient()
+	if err != nil {
+		return err
+	}
+	clubID := c.ID.String()
+	byteData, _ := json.Marshal(fiber.Map{
+		"action": "Club Update",
+		"data":   c,
+	})
+	rdb.Publish(ctx, clubID, byteData)
+	return nil
 }
--- models/clubuser.go
+++ models/clubuser.go
@@ -1,9 +1,52 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"context"
+	"encoding/json"
+	"github.com/Krishap-s/keats-backend/redisclient"
+	"github.com/go-pg/pg/v10"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
 
 type ClubUser struct {
 	ID     uuid.UUID `pg:",pk,type:uuid,default:uuid_generate_v4()" json:"id"`
 	ClubID uuid.UUID `pg:"type:uuid,nopk,notnull,unique:clubuser" json:"room_id"`
 	UserID uuid.UUID `pg:"type:uuid,nopk,notnull,unique:clubuser" json:"user_id"`
+}
+
+var _ pg.AfterInsertHook = (*ClubUser)(nil)
+
+// AfterInsert hook publishes to websocket clients that a user has joined the club
+func (c *ClubUser) AfterInsert(ctx context.Context) error {
+	rdb, err := redisclient.GetRedisClient()
+	if err != nil {
+		return err
+	}
+	userID := c.UserID.String()
+	clubID := c.ClubID.String()
+	byteData, _ := json.Marshal(fiber.Map{
+		"action": "User Join",
+		"data":   userID,
+	})
+	rdb.Publish(ctx, clubID, byteData)
+	return nil
+}
+
+var _ pg.AfterDeleteHook = (*ClubUser)(nil)
+
+// AfterDelete hook publishes to websocket clients that a user has left the club
+func (c *ClubUser) AfterDelete(ctx context.Context) error {
+	rdb, err := redisclient.GetRedisClient()
+	if err != nil {
+		return err
+	}
+	userID := c.UserID.String()
+	clubID := c.ClubID.String()
+	byteData, _ := json.Marshal(fiber.Map{
+		"action": "User Leave",
+		"data":   userID,
+	})
+	rdb.Publish(ctx, clubID, byteData)
+	return nil
 }
